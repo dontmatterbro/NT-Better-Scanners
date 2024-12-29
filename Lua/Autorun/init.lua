b8a4c5aaf015910c23f8dd5a6d6763f2db2c0aa3
I only use this when bored <yh846175@gmail.com>
2024-12-29 22:56:59 +0300

    last update

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,11 +1,17 @@
 NTSCAN = {} -- Neurotrauma Better Scanner
 NTSCAN.Name="Better Scanner"
-NTSCAN.Version = "A1.0.1h3"
-NTSCAN.VersionNum = 01000103
+NTSCAN.Version = "A1.0.1h4"
+NTSCAN.VersionNum = 01000104
 NTSCAN.MinNTVersion = "A1.9.4h1"
 NTSCAN.MinNTVersionNum = 01090401
 NTSCAN.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil then NTC.RegisterExpansion(NTSCAN) end end,1)
+
+if 
+	NT.VersionNum >= 01110000 
+then 
+	print("NT Better Scanner has been integrated into base Neurotrauma and has been disabled to prevent conflicts.")
+return end
 
 Timer.Wait(function()
 	if (SERVER or (CLIENT and not Game.IsMultiplayer)) and (NTC==nil) then --checks if NT is installed
